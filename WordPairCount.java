8207b5d609d4fca793c518e45e8c13774bfe0bd5
manish-nagdevani <manish.nagdevani8@gmail.com>
2018-02-23 17:10:57 -0500

    Adding Map Reduce Job - Java Code

---
+++ WordPairCount.java
@@ -0,0 +1,111 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class WordPairCount  extends Configured implements Tool{
+
+    /**
+     * Main function which calls the run method and passes the args using ToolRunner
+     * @param args Two arguments input and output file paths
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new WordPairCount(), args);
+        System.exit(exitCode);
+    }
+
+    /**
+     * Run method which schedules the Hadoop Job
+     * @param args Arguments passed in main function
+     */
+    public int run(String[] args) throws Exception {
+
+        if (args.length != 2) {
+            System.err.printf("Usage: %s needs two arguments <input> <output> files\n",
+                    getClass().getSimpleName());
+            return -1;
+        }
+
+        //Initialize the Hadoop job and set the jar as well as the name of the Job
+        Job job = new Job();
+        job.setJarByClass(WordPairCount.class);
+        job.setJobName("WordPairCounter");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        //Set the MapClass and ReduceClass in the job
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(IntSumReducer.class);
+
+        //Wait for the job to complete and print if the job was successful or not
+        int returnValue = job.waitForCompletion(true) ? 0:1;
+
+        if(job.isSuccessful()) {
+            System.out.println("Job was successful");
+        } else if(!job.isSuccessful()) {
+            System.out.println("Job was not successful");
+        }
+
+        return returnValue;
+    }
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+        	String delim = "[^a-z A-Z 0-9]+";
+        	String validValue = value.toString().replaceAll(delim, " ");
+        	String word1 = null, word2 = null;
+            StringTokenizer itr = new StringTokenizer(validValue, " ");
+            
+            if(itr.hasMoreTokens()) {
+            	word1 = new String(itr.nextToken().trim());
+            }
+            
+            while (itr.hasMoreTokens()) {
+				word2 = new String(itr.nextToken().trim());
+				if(!word1.isEmpty() && !word2.isEmpty()) {
+					Text outKey = new Text(word1+ " " + word2);
+					IntWritable outVal = new IntWritable(1);
+					context.write(outKey, outVal);
+					word1 = new String(word2);
+				}
+            }
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+}
+
